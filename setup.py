e1b014fb921d414f97ee5c95949566f286668f23
Daniel Silva <danielnsilva@gmail.com>
2021-10-31 11:56:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='semanticscholar',
-    version='0.1.6',
+    version='0.2.0',
     description='A python library that aims to retrieve data from Semantic Scholar API',
     long_description=long_description,
     long_description_content_type="text/markdown",
