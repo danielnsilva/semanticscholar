ceb9910d9bab5e25d4070ea39319e73db59ded39
Daniel Silva <danielnsilva@gmail.com>
2021-11-28 18:47:19 -0500

    Update Author and Paper attributes

--- semanticscholar/Author.py
+++ semanticscholar/Author.py
@@ -1,46 +1,72 @@
 class Author:
 
-    def __init__(self, data):
+    def __init__(self, data) -> None:
         self._init_attributes(data)
 
     @property
-    def aliases(self):
+    def affiliations(self) -> list:
+        return self._affiliations
+
+    @property
+    def aliases(self) -> list:
         return self._aliases
 
     @property
-    def authorId(self):
+    def authorId(self) -> str:
         return self._authorId
+    
+    @property
+    def citationCount(self) -> int:
+        return self._citationCount
 
     @property
-    def influentialCitationCount(self):
-        return self._influentialCitationCount
+    def externalIds(self) -> dict:
+        return self._externalIds
 
     @property
-    def name(self):
+    def hIndex(self) -> int:
+        return self._hIndex
+
+    @property
+    def homepage(self) -> str:
+        return self._homepage
+
+    @property
+    def name(self) -> str:
         return self._name
 
     @property
-    def papers(self):
+    def paperCount(self) -> int:
+        return self._paperCount
+
+    @property
+    def papers(self) -> list:
         return self._papers
 
     @property
-    def url(self):
+    def url(self) -> str:
         return self._url
-
-    @property
-    def data(self):
-        return self._data
 
     def _init_attributes(self, data):
         self._data = data
+        if 'affiliations' in data:
+            self._affiliations = data['affiliations']
         if 'aliases' in data:
             self._aliases = data['aliases']
         if 'authorId' in data:
             self._authorId = data['authorId']
-        if 'influentialCitationCount' in data:
-            self._influentialCitationCount = data['influentialCitationCount']
+        if 'citationCount' in data:
+            self._citationCount = data['citationCount']
+        if 'externalIds' in data:
+            self._externalIds = data['externalIds']
+        if 'hIndex' in data:
+            self._hIndex = data['hIndex']
+        if 'homepage' in data:
+            self._homepage = data['homepage']
         if 'name' in data:
             self._name = data['name']
+        if 'paperCount' in data:
+            self._paperCount = data['paperCount']
         if 'papers' in data:
             self._papers = data['papers']
         if 'url' in data:
--- semanticscholar/Paper.py
+++ semanticscholar/Paper.py
@@ -8,28 +8,24 @@ class Paper:
         return self._abstract
 
     @property
-    def arxivId(self) -> str:
-        return self._arxivId
-
-    @property
     def authors(self) -> list:
         return self._authors
 
     @property
-    def citationVelocity(self) -> int:
-        return self._citationVelocity
+    def citationCount(self) -> int:
+        return self._citationCount
 
     @property
-    def citations(self) -> int:
+    def citations(self) -> list:
         return self._citations
 
     @property
-    def corpusId(self) -> str:
-        return self._corpusId
+    def embedding(self) -> dict:
+        return self._embedding
 
     @property
-    def doi(self) -> str:
-        return self._doi
+    def externalIds(self) -> dict:
+        return self._externalIds
 
     @property
     def fieldsOfStudy(self) -> list:
@@ -44,28 +40,12 @@ class Paper:
         return self._isOpenAccess
 
     @property
-    def isPublisherLicensed(self) -> bool:
-        return self._isPublisherLicensed
-
-    @property
-    def is_open_access(self) -> bool:
-        return self._isOpenAccess
-
-    @property
-    def is_publisher_licensed(self) -> bool:
-        return self._isPublisherLicensed
-
-    @property
-    def numCitedBy(self) -> int:
-        return self._numCitedBy
-
-    @property
-    def numCiting(self) -> int:
-        return self._numCiting
-
-    @property
     def paperId(self) -> str:
         return self._paperId
+
+    @property
+    def referenceCount(self) -> int:
+        return self._referenceCount
 
     @property
     def references(self) -> list:
@@ -76,8 +56,8 @@ class Paper:
         return self._title
 
     @property
-    def topics(self) -> list:
-        return self._topics
+    def tldr(self) -> dict:
+        return self._tldr
 
     @property
     def url(self) -> str:
@@ -95,42 +75,32 @@ class Paper:
         self.data = data
         if 'abstract' in data:
             self._abstract = data['abstract']
-        if 'arxivId' in data:
-            self._arxivId = data['arxivId']
         if 'authors' in data:
             self._authors = data['authors']
-        if 'citationVelocity' in data:
-            self._citationVelocity = data['citationVelocity']
+        if 'citationCount' in data:
+            self._citationCount = data['citationCount']
         if 'citations' in data:
             self._citations = data['citations']
-        if 'corpusId' in data:
-            self._corpusId = data['corpusId']
-        if 'doi' in data:
-            self._doi = data['doi']
+        if 'embedding' in data:
+            self._embedding = data['embedding']
+        if 'externalIds' in data:
+            self._externalIds = data['externalIds']
         if 'fieldsOfStudy' in data:
             self._fieldsOfStudy = data['fieldsOfStudy']
         if 'influentialCitationCount' in data:
             self._influentialCitationCount = data['influentialCitationCount']
         if 'isOpenAccess' in data:
             self._isOpenAccess = data['isOpenAccess']
-        if 'isPublisherLicensed' in data:
-            self._isPublisherLicensed = data['isPublisherLicensed']
-        if 'is_open_access' in data:
-            self._is_open_access = data['is_open_access']
-        if 'is_publisher_licensed' in data:
-            self._is_publisher_licensed = data['is_publisher_licensed']
-        if 'numCitedBy' in data:
-            self._numCitedBy = data['numCitedBy']
-        if 'numCiting' in data:
-            self._numCiting = data['numCiting']
         if 'paperId' in data:
             self._paperId = data['paperId']
+        if 'referenceCount' in data:
+            self._referenceCount = data['referenceCount']
         if 'references' in data:
             self._references = data['references']
         if 'title' in data:
             self._title = data['title']
-        if 'topics' in data:
-            self._topics = data['topics']
+        if 'tldr' in data:
+            self._tldr = data['tldr']
         if 'url' in data:
             self._url = data['url']
         if 'venue' in data:
