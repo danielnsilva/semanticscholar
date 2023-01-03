63acfcd6d3284c4e870f06e50ad263dfab3a42f0
Daniel Silva <danielnsilva@gmail.com>
2023-01-03 10:46:25 -0500

    refactor: revert timeout parameter change

--- semanticscholar/SemanticScholar.py
+++ semanticscholar/SemanticScholar.py
@@ -48,20 +48,20 @@ class SemanticScholar:
         self._timeout = timeout
         self._requester = ApiRequester(self._timeout)
 
-    def get_timeout(self) -> int:
+    @property
+    def timeout(self) -> int:
         '''
         :type: :class:`int`
         '''
         return self._timeout
 
-    def set_timeout(self, timeout: int):
+    @timeout.setter
+    def timeout(self, timeout: int) -> None:
         '''
         :param int timeout:
         '''
         self._timeout = timeout
         self._requester.timeout = timeout
-
-    timeout = property(get_timeout, set_timeout)
 
     def get_paper(
                 self,
