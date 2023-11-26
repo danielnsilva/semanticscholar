3161e85f08e87aba60a4d6ac7ac329317e435508
Daniel Silva <danielnsilva@gmail.com>
2023-11-26 16:36:34 +0000

    build: :heavy_plus_sign: :heavy_minus_sign: update requirements

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     license='MIT',
     packages=['semanticscholar'],
     python_requires='>=3.7',
-    install_requires=['requests', 'tenacity'],
+    install_requires=['tenacity', 'httpx', 'nest_asyncio'],
     test_suite='tests',
     tests_require=['vcrpy'],
     classifiers=[
@@ -25,7 +25,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
