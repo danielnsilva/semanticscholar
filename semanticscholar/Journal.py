6ecbabd700ba4be17ed0c6ede240d1e79c045620
Daniel Silva <danielnsilva@gmail.com>
2024-04-01 21:00:37 +0000

    refactor: Journal and Tldr inherit from SemanticScholarObject

--- semanticscholar/Journal.py
+++ semanticscholar/Journal.py
@@ -1,28 +1,19 @@
 from typing import Any
 
+from semanticscholar.SemanticScholarObject import SemanticScholarObject
 
-class Journal:
+
+class Journal(SemanticScholarObject):
     '''
     This class represents the Journal where the paper was published.
     '''
 
     def __init__(self, data) -> None:
+        super().__init__()
         self._name = None
         self._pages = None
         self._volume = None
         self._init_attributes(data)
-
-    def __str__(self) -> str:
-        return f'{self._name}'
-
-    def __repr__(self) -> str:
-        return self.__str__()
-
-    def __getitem__(self, key) -> Any:
-        return self._data.__getitem__(key)
-
-    def keys(self):
-        return self._data.keys()
 
     @property
     def name(self) -> str:
@@ -44,13 +35,6 @@ class Journal:
         :type: :class:`int`
         '''
         return self._volume
-
-    @property
-    def raw_data(self) -> dict:
-        '''
-        :type: :class:`dict`
-        '''
-        return self._data
 
     def _init_attributes(self, data):
         self._data = data
--- semanticscholar/Tldr.py
+++ semanticscholar/Tldr.py
@@ -1,28 +1,19 @@
 from typing import Any
 
+from semanticscholar.SemanticScholarObject import SemanticScholarObject
 
-class Tldr:
+
+class Tldr(SemanticScholarObject):
     '''
     This class represents auto-generated short summary of the paper from the
     SciTLDR model.
     '''
 
     def __init__(self, data) -> None:
+        super().__init__()
         self._model = None
         self._text = None
         self._init_attributes(data)
-
-    def __str__(self) -> str:
-        return self._text
-
-    def __repr__(self) -> str:
-        return self.__str__()
-
-    def __getitem__(self, key) -> Any:
-        return self._data.__getitem__(key)
-
-    def keys(self):
-        return self._data.keys()
 
     @property
     def model(self) -> str:
@@ -37,13 +28,6 @@ class Tldr:
         :type: :class:`str`
         '''
         return self._text
-
-    @property
-    def raw_data(self) -> dict:
-        '''
-        :type: :class:`dict`
-        '''
-        return self._data
 
     def _init_attributes(self, data):
         self._data = data
