73c77266abcab52125941736d87c659ed4fa0e15
Daniel Silva <danielnsilva@gmail.com>
2026-03-30 01:29:22 +0000

    refactor: remove unused imports

--- semanticscholar/Journal.py
+++ semanticscholar/Journal.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 from semanticscholar.SemanticScholarObject import SemanticScholarObject
 
 
--- tests/test_semanticscholar.py
+++ tests/test_semanticscholar.py
@@ -1,5 +1,4 @@
 import json
-import logging
 import unittest
 from datetime import datetime
 from unittest import mock
